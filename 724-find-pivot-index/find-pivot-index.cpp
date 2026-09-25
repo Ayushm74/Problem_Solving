1a68765caf3bce915ddd9747ec80dfebab26920c
Ayush Mishra <Ayushmishra74992@gmail.com>
2026-09-25 18:34:42 +0530

    Time: 0 ms (100.00%) | Memory: 36.9 MB (15.90%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pref(n);
+        pref[0] = nums[0];
+        vector<int> s(n);
+        s[n-1] = nums[n-1];
+        for(int i = 1;i<n;i++){
+            pref[i] = pref[i-1] + nums[i];
+        }
+        
+        for(int i = n-2;i>=0;i--){
+            s[i] = s[i+1] + nums[i];
+        }
+        for(int i = 0;i<n;i++){
+            if(pref[i] - s[i] == 0) return i; 
+        }
+        return -1;
+        
+        
+        
+    }
+};
