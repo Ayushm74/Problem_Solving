4f5eb883e8fe046a3c3aaf0ac81a4684ee8f4302
Ayush Mishra <Ayushmishra74992@gmail.com>
2026-09-25 18:54:56 +0530

    Time: 0 ms (100.00%) | Memory: 37.1 MB (10.14%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
-        vector<int> pref(n);
+        vector<int> pref(n+1);
         pref[0] = nums[0];
-        vector<int> s(n);
+        vector<int> s(n+1);
         s[n-1] = nums[n-1];
         for(int i = 1;i<n;i++){
             pref[i] = pref[i-1] + nums[i];
